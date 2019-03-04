b6b472b8c30026ed2b11c3f3c6b52ebff73e3dfe
tower1229 <tower1229@foxmail.com>
2019-03-04 13:44:44 +0800

    强制更新

--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
 //app.js
 App({
+  upDataApp: function () {//版本更新
+    const updateManager = wx.getUpdateManager();
+    updateManager.onCheckForUpdate(function (res) {
+      if (res.hasUpdate) { // 请求完新版本信息的回调
+        updateManager.onUpdateReady(function () {
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用？',
+            success: function (res) {
+              if (res.confirm) {
+                updateManager.applyUpdate()
+              }
+            }
+          })
+        });
+        updateManager.onUpdateFailed(function () {
+          wx.showModal({// 新的版本下载失败
+            title: '已经有新版本了哟~',
+            content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开',
+          })
+        })
+      }else{
+        console.warn(res)
+      }
+    })
+  },
   onLaunch: function() {
     //初始化云环境
     wx.cloud.init({
@@ -20,6 +46,9 @@ App({
     //   enableDebug: true
     // })
 
+  },
+  onShow: function(){
+    this.upDataApp()
   },
   globalData: {
     userInfo: null,
